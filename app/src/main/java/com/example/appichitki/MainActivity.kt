a06d010a212ea9d6bf6042228a366bab3ece41e6
Edherrera <edherrera96@uan.edu.co>
2021-09-27 18:59:45 -0500

    se agrego ir a otra actividad

--- app/src/main/java/com/example/appichitki/MainActivity.kt
+++ app/src/main/java/com/example/appichitki/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.appichitki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnSecond = findViewById<Button>(R.id.btnSecondActivity)
+
+
+        btnSecond.setOnClickListener { goToSecondActivity() }
+
+
     }
+
+    fun goToSecondActivity() {
+        val secondIntent = Intent(this,MainActivity2::class.java)
+        secondIntent.putExtra("MISDATOS", "El valor que quiero pasar a la otra actividad")
+        startActivity(secondIntent)
+    }
+
 }
