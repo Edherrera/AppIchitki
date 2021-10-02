9ed08cf13cc66459067caa46355c59524f63e019
Edherrera <edherrera96@uan.edu.co>
2021-10-02 15:30:42 -0500

    se agrego la tecera actividad

--- app/src/main/java/com/example/appichitki/MainActivity2.kt
+++ app/src/main/java/com/example/appichitki/MainActivity2.kt
@@ -1,13 +1,27 @@
 package com.example.appichitki
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val data = intent.getStringExtra("MISDATOS")
+        val btnSecond = findViewById<Button>(R.id.btnActividadTres)
+
+        btnSecond.setOnClickListener { goToSecondActivity() }
+
+
+    }
+
+    fun goToSecondActivity() {
+        val secondIntent = Intent(this,MainActivity3::class.java)
+        secondIntent.putExtra("MISDATOS", "El valor que quiero pasar a la otra actividad")
+        startActivity(secondIntent)
+        Toast.makeText(this, "Bienvenido a Hoteles de Uribia!!!", Toast.LENGTH_LONG).show()
     }
 }
