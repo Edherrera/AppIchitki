f0165fb608e89f45764d6325c765d42bf7487abe
Edherrera <edherrera96@uan.edu.co>
2021-10-28 23:39:41 -0500

    se agrego servicios de hoteles

--- app/src/main/java/com/example/appichitki/MainActivity2.kt
+++ app/src/main/java/com/example/appichitki/MainActivity2.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
@@ -12,10 +13,19 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         val btnSecond = findViewById<Button>(R.id.btnActividadTres)
+        val imaguri = findViewById<ImageView>(R.id.imagurib)
 
         btnSecond.setOnClickListener { goToSecondActivity() }
+        imaguri.setOnClickListener { iraUribia() }
 
 
+    }
+
+    fun iraUribia() {
+        val secondIntent = Intent(this,MainJuyasirain::class.java)
+        secondIntent.putExtra("MISDATOS", "El valor que quiero pasar a la otra actividad")
+        startActivity(secondIntent)
+        Toast.makeText(this, "Bienvenido al Hotel Juyasirain!!!", Toast.LENGTH_LONG).show()
     }
 
     fun goToSecondActivity() {
---
+++ app/src/main/java/com/example/appichitki/MainJuyasirain.kt
@@ -0,0 +1,30 @@
+package com.example.appichitki
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainJuyasirain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_juyasirain)
+        val btnImplicit = findViewById<Button>(R.id.btnImplicit)
+
+
+        btnImplicit.setOnClickListener { goToOtherApp() }
+    }
+
+
+
+
+
+
+
+    fun goToOtherApp() {
+        val implicitIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:+573002353263"))
+        //implicitIntent.putExtra("sms_body","Mi mensaje")
+        startActivity(implicitIntent)
+    }
+}
